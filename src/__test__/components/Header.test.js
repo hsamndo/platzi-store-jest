d44536c5e1f35dc61223aacf170cc3d41f6868fa
hsamndo <g16ada13@hackademy.mx>
2021-10-13 00:23:04 -0500

    project

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import ProviderMock from '../../__mocks__/providerMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('Render del componente Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('Render del título', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshop', () => {
+  test('Comprobar el snapshopt de header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
